10a27eb386020c582118919d34e6ddcd7a7bb4c9
Brett T. Warden <bwarden@cpan.org>
2025-10-17 13:35:32 -0700

    Implement SVG::Barcode::EAN8

--- t/resources/mk_test_files.pl
+++ t/resources/mk_test_files.pl
@@ -73,3 +73,25 @@ use lib qw(../../lib);
     close($fh_red) or warn "Cannot close ${code}_red_notext.svg: $!";
 }
 
+{
+    my $code = '12345670';
+    use SVG::Barcode::EAN8;
+
+    my $obj = SVG::Barcode::EAN8->new;
+
+    my $svg_black = $obj->plot($code);
+
+    $obj->foreground('red');
+    $obj->textsize(0);
+    $obj->lineheight(20);
+
+    my $svg_red = $obj->plot($code);
+
+    open(my $fh_black, '>', "${code}_black_text.svg") or die "Cannot open ${code}_black_text.svg: $!";
+    print $fh_black $svg_black;
+    close($fh_black) or warn "Cannot close ${code}_black_text.svg: $!";
+
+    open(my $fh_red, '>', "${code}_red_notext.svg") or die "Cannot open ${code}_red_notext.svg: $!";
+    print $fh_red $svg_red;
+    close($fh_red) or warn "Cannot close ${code}_red_notext.svg: $!";
+}
